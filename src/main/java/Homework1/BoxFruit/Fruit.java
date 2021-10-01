d537f657b14240bd11739532a5c7e0441e65ae9b
Gudkova Lena <gudkova_elena1992@mail.ru>
2021-10-01 04:38:02 +0300

    Homework1

---
+++ src/main/java/Homework1/BoxFruit/Fruit.java
@@ -0,0 +1,12 @@
+package Homework1.BoxFruit;
+import java.lang.reflect.Type;
+
+abstract public class Fruit {
+    private static float weight;
+
+
+    public float getWeight() {
+        return weight;
+    }
+
+}
